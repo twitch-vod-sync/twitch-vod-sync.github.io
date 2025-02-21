5fa060065d4d9df0310bd140aafdd526d42447bc
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-02-21 06:38:07 -0800

    I'm not using this so I'm hiding it

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -256,7 +256,7 @@ export class Viewer extends React.PureComponent<ViewerProps, ViewerState> {
           }}
         >
           <div id={'player' + this.props.id} className="player"></div>
-          <div className="options">
+          <div className="options" style="display:none">
             <form onSubmit={this.handleDelayChange}>
               Delay:{' '}
               <input
