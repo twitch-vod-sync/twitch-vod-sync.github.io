06239e27c3c4522ff7fc59a7353437705c96abd9
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-06-07 01:17:31 -0700

    Update tests.py
    ???

--- tests.py
+++ tests.py
@@ -165,8 +165,9 @@ class UITests:
     # Seek on the first player, then quickly seek again on the last player. Since players seek in order (probably?) the last players' seeking won't be done.
     # Simulate a user's seek by using the internal player.
     self.run('''
-      players.get("player1")._player.seek(60.0)
-      setTimeout(() => players.get("player9")._player.seek(120.0), 10)
+      players.get("player0")._player.seek(60.0)
+      setTimeout(() => players.get("player0")._player.seek(60.0), 5)
+      setTimeout(() => players.get("player9")._player.seek(120.0), 15)
     ''')
 
     # For a while, this caused a nasty thrashing bug, where the two seek values would keep getting hot-potatoed around between players.
